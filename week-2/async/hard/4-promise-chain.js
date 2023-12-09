c15d004ee77fb0b425b934f92095106b31cef689
Lokendra <anizen242@gmail.com>
2023-12-10 04:21:18 +0530

    added time difference

--- week-2/async/hard/4-promise-chain.js
+++ week-2/async/hard/4-promise-chain.js
@@ -51,3 +51,5 @@ customPromiseAll();
 
 //comparing 3 and 4, we observed that Promise.all takes less time to resolve all three promises this is because while sequentially exeucting promises, we are waiting for the promise to get resolved and only then we're moving onto the next promise.
 //promise.all involves parallelism while promise-chain involves sequential execution.
+
+//there was a 3s time difference
